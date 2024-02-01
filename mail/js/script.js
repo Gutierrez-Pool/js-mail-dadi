a1ed52a81c9a806a026dd9cb70e9d4faa8825726
Pool-Gutierrez <pool.gutierrezv@gmail.com>
2024-02-01 21:27:49 +0100

    mail

---
+++ mail/js/script.js
@@ -0,0 +1,26 @@
+// Mail
+
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere (qui quindi dovremmo creare da noi un array di email casuali),
+// stampa un messaggio appropriato sull’esito del controllo.
+// (NON utilizzate il metodo .includes())
+// Prima di partire a scrivere codice poniamoci qualche domanda:
+// Che ci sia un array da qualche parte?
+// Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+
+// Consigli del giorno:
+// 1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+// 2. javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
+// 3. si ma noi cosa vogliamo fare?
+// 4. torniamo a scrivere in italiano
+// 5. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+
+
+// array mail
+const mails= ["primo@gmail.com", "secondo@gmail.com", "terzo@gmail.com", "quarto@gmail.com", "quinto@gmail.com", "sesto@gmail.com", "settimo@gmail.com", "ottavo@gmail.com", "nono@gmail.com", "decimo@gmail.com"];
+
+const userMailElement = document.querySelector("#user-mail");
+
+
+
+// console.log(mails)
